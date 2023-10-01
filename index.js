2604cee57004939a422474d857e34d0449f921b3
Divas-Sagta <divassagta19@gmail.com>
2023-10-01 18:13:32 +0530

    second-commit

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const express= require('express')
 const app= express()
 
 const port=4000
-
+const info={
+    "NAME" : "DIVAS SAGTA",
+    "AGE" :  20,
+    "EDUCATION" : "CSE UNDERGRAD"
+}
 
 app.get('/',(req,res)=>{
     res.send("Hello World!")
@@ -12,6 +16,10 @@ app.get('/',(req,res)=>{
 
 app.get('/login',(req,res)=>{
     res.send('<h1>Login Successfully!</h1>')
+})
+
+app.get('/info',(req,res)=>{
+    res.send(info)
 })
 
 app.listen(process.env.PORT,()=>{
