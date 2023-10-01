dbc98d2bef0bd1b9edb2fb6c591781abe19c4f7c
Divas-Sagta <divassagta19@gmail.com>
2023-10-01 17:44:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express= require('express')
+const app= express()
+
+const port=4000
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello World!")
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login Successfully!</h1>')
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log(`app is listening on port ${port}`)
+})
